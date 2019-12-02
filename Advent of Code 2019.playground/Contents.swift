e730ffee7887364494b8b4eac1e17fbec2a773f2
James Porter <james@amimetic.co.uk>
2019-12-02 23:23:29 +0000

    day 2

--- Advent of Code 2019.playground/Contents.swift
+++ Advent of Code 2019.playground/Contents.swift
@@ -1,11 +1,18 @@
 import Cocoa
 
-
-func readNumbers(filename: String) throws -> [Int] {
+// MARK: Helpers: going to do these kinds of things a lot judging from previous years!
+func readNumbersFromLines(filename: String) -> [Int] {
     let fileURL = Bundle.main.url(forResource: filename, withExtension: "txt")
-    let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+    let content = try! String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
     
     return content.split(separator: "\n").compactMap { Int(String($0)) }
+}
+
+func readNumberList(filename: String) -> [Int] {
+    let fileURL = Bundle.main.url(forResource: filename, withExtension: "txt")
+    let content = try! String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+    
+    return content.split(separator: ",").compactMap { Int(String($0)) }
 }
 
 extension Double {
@@ -21,29 +28,84 @@ extension Int {
 }
 
 
-let oneData = try readNumbers(filename: "1")
+//let oneData = readNumbersFromLines(filename: "1")
 
 //1a Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2. What is all fuel?
 
-let oneAAns = oneData.map { n in
-    floor(n.d / 3.0) - 2
-}.reduce(0.0, +)
+//let oneAAns = oneData.map { n in
+//    floor(n.d / 3.0) - 2
+//}.reduce(0.0, +)
+//
+//print("1a: \(oneAAns.ri)")
+//
+//func fuel1b(n: Int) -> Int {
+//    let baseFuel = (floor(n.d / 3.0) - 2).ri
+//    if baseFuel > 0 {
+//        return baseFuel + fuel1b(n: baseFuel)
+//    } else {
+//        return 0
+//    }
+//}
+//
+//let oneBAns = oneData.map(fuel1b).reduce(0, +)
+//print("1b: \(oneBAns)")
 
-print("1a: \(oneAAns.ri)")
+// https://adventofcode.com/2019/day/2
+var twoData = readNumberList(filename: "2")
 
-func fuel1b(n: Int) -> Int {
-    let baseFuel = (floor(n.d / 3.0) - 2).ri
-    if baseFuel > 0 {
-        return baseFuel + fuel1b(n: baseFuel)
-    } else {
-        return 0
+func evaluate(program: inout [Int]) {
+    var i = 0
+    mainLoop: while true {
+        switch program[i] {
+        case 1:
+            let a = program[program[i + 1]]
+            let b = program[program[i + 2]]
+            let j = program[i + 3]
+            program[j] = a + b
+            i += 4
+        case 2:
+            let a = program[program[i + 1]]
+            let b = program[program[i + 2]]
+            let j = program[i + 3]
+            program[j] = a * b
+            i += 4
+        case 99:
+            print("Done! at \(i)")
+            break mainLoop
+        default:
+            print("Error")
+            break mainLoop
+        }
     }
 }
 
-let oneBAns = oneData.map(fuel1b).reduce(0, +)
-print("1b: \(oneBAns)")
 
+//var example = [ 2,4,4,5,99,0]
+//evaluate(program: &example)
+//print(example)
+//
+//var example2 = [1,1,1,4,99,5,6,0,99]
+//evaluate(program: &example2)
+//print(example2)
 
+// 2a
+//twoData[1] = 12
+//twoData[2] = 2
+//evaluate(program: &twoData)
+//print("2a \(twoData[0])")
 
+// 2b (is a bit slow [76,21]; ans: 7621)
 
-
+outer: for i in 0...99 {
+    for j in 0...99 {
+        var twoData = readNumberList(filename: "2")
+        twoData[1] = i
+        twoData[2] = j
+        evaluate(program: &twoData)
+        if twoData[0] == 19690720 {
+            print("2b [\(i),\(j)]; ans\(100 * i + j)")
+            break outer
+        }
+        
+    }
+}
---
+++ Advent of Code 2019.playground/Sources/Helpers.swift
@@ -0,0 +1,3 @@
+import Cocoa
+
+// hmm... I put stuff here but not imported? come back to?
