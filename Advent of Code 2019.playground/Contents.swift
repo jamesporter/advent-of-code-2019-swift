4c5316f37248c4b4a17e3641ac310e20bfc2443a
James Porter <james@amimetic.co.uk>
2019-12-09 23:13:19 +0000

    8

--- Advent of Code 2019.playground/Contents.swift
+++ Advent of Code 2019.playground/Contents.swift
@@ -190,34 +190,138 @@ extension Int {
 //}
 //print("4b \(count)")
 
-let rawSix = readFile(name: "6")
-// from orbiter to parent
-var lookup: [String: String] = [:]
-var keys = Set<String>()
+//let rawSix = readFile(name: "6")
+//// from orbiter to parent
+//var lookup: [String: String] = [:]
+//var reverseLookup: [String: [String]] = [:]
+//var keys = Set<String>()
+//
+//rawSix.split(separator: "\n").forEach {
+//    let items = $0.split(separator: ")")
+//    let a = String(items[0])
+//    let b = String(items[1])
+//    lookup[b] = a
+//    if (reverseLookup[a] != nil) {
+//        reverseLookup[a] = reverseLookup[a]! + [b]
+//    } else {
+//        reverseLookup[a] = [b]
+//    }
+//
+//    keys.insert(a)
+//    keys.insert(b)
+//}
 
-rawSix.split(separator: "\n").forEach {
-    let items = $0.split(separator: ")")
-    let a = String(items[0])
-    let b = String(items[1])
-    lookup[b] = a
-    keys.insert(a)
-    keys.insert(b)
-}
+//var orbits = 0
+//keys.forEach {
+//    var current: String? = $0
+//    while current != nil {
+//        let nextOne = lookup[current!]
+//        if nextOne != nil {
+//            orbits += 1
+//            current = nextOne
+//        } else {
+//            current = nil
+//        }
+//    }
+//}
+//
+//print("6a \(orbits)")
 
-var orbits = 0
-keys.forEach {
-    var current: String? = $0
-    while current != nil {
-        let nextOne = lookup[current!]
-        if nextOne != nil {
-            orbits += 1
-            current = nextOne
-        } else {
-            current = nil
+// shortest path... start somewhere, bfs, stop?
+
+//var visited = Set<String>()
+//var frontier = Set<String>(["YOU"])
+//
+//func neighbours(_ places: Set<String>) -> Set<String> {
+//    var nbrs = Set<String>()
+//    places.forEach {
+//        if let parent = lookup[$0] {
+//            nbrs.insert(parent)
+//        }
+//        if let children = reverseLookup[$0] {
+//            children.forEach { c in
+//                nbrs.insert(c)
+//            }
+//        }
+//    }
+//    return nbrs
+//}
+//
+//var foundSanta = false
+//var count = 0
+//
+//while !foundSanta {
+//    count += 1
+//    let newNbrs = neighbours(frontier).subtracting(visited)
+//    if newNbrs.contains("SAN") {
+//        foundSanta = true
+//    } else {
+//        frontier = newNbrs
+//        visited.formUnion(newNbrs)
+//    }
+//}
+//
+//// offset by 2 as per weird formulation
+//print("6b \(count - 2)")
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
 }
 
-print("6a \(orbits)")
+let rawEight = readFile(name: "8")
+let digits = rawEight.compactMap { Int(String($0)) }
+print(digits.count)
+
+// layers of 25x6 = 150
+let layers = digits.chunked(into: 150)
+
+//let zeros = layers.map { layer in
+//    layer.filter { $0 == 0}.count
+//}
+//
+//let i = zeros.firstIndex(of: zeros.min()!)!
+//let oneC = layers[i].filter { $0 == 1 }.count
+//let twoC = layers[i].filter { $0 == 2 }.count
+//
+//print("8a \(oneC * twoC)")
+
+var image: [Int] = []
+
+for i in 0..<150 {
+    var value = 2
+    var j = 0
+    repeat {
+        value = layers[j][i]
+        j += 1
+    } while value == 2
+    image.append(value)
+}
+
+let rows = image.chunked(into: 25)
+
+rows.forEach { r in
+    print( r.map { $0 == 0 ? "X" : " " }.joined() )
+}
+
+/*
+    X XX X XX XX  XXXX  X
+XXX X XX X X XX XX XXXX X
+XX XX XX X  XXX XXXXXXX X
+X XXX XX X X XX XXXXXXX X
+ XXXX XX X X XX XX X XX X
+    XX  XX XX XX  XXX  XX
+ */
+
+
+
+
+
+
+
+
 
 
